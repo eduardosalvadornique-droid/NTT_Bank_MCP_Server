d2351e5f55b173bbb56f6d7fed1be8bd148e1351
Eduardo Salvador Ñ. <eduardosalvadornique@gmail.com>
2026-03-03 16:29:21 -0500

    Create server.py

---
+++ server.py
@@ -0,0 +1,35 @@
+from fastmcp import FastMCP
+
+from tools import rag, range_earnings, benefits, identification, cards
+from resources import range_earnings as range_earnings_resources
+from resources import benefits as benefits_resources
+from resources import identification as identification_resources
+from resources import cards as cards_resources
+
+# =========================================================
+# Servidor MCP
+# =========================================================
+mcp = FastMCP("NTT Bank")
+
+# =========================================================
+# Registro de Tools
+# =========================================================
+rag.register(mcp)
+range_earnings.register(mcp)
+benefits.register(mcp)
+identification.register(mcp)
+cards.register(mcp)
+
+# =========================================================
+# Registro de Resources
+# =========================================================
+range_earnings_resources.register(mcp)
+benefits_resources.register(mcp)
+identification_resources.register(mcp)
+cards_resources.register(mcp)
+
+# =========================================================
+# Run
+# =========================================================
+if __name__ == "__main__":
+    mcp.run()
